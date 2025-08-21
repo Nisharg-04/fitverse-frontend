61a2c02a7acdb3c7af1112e8c2fdbcc8f64532e6
Nisharg Soni <ncsoni9898@gmail.com>
2025-08-21 19:09:55 +0530

    mani

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.38ts1sa06ng"
+    "revision": "0.9vfrpjfs85"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
